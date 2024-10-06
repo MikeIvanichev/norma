474a12763efd9e839299c9084f239805c1637e14
Mike <42689366+MikeIvanichev@users.noreply.github.com>
2024-10-07 00:56:28 +0300

    Fix #23
    Fix #23
    Account for case of -inf predictions for all timestamps between 0 and 1 for first token

--- src/models/whisper/model.rs
+++ src/models/whisper/model.rs
@@ -304,6 +304,16 @@ impl Model {
                 .to_scalar::<f32>()? as f64
         };
 
+        // If the no speech prob is over the threshold we can return early
+        if no_speech_prob > m::NO_SPEECH_THRESHOLD {
+            return Ok(DecodingResult {
+                tokens,
+                avg_logprob: 0.0, // We havent decoded any tokens yet
+                no_speech_prob,
+                compression_ratio: f64::NAN,
+            });
+        }
+
         while tokens.last().unwrap() != &self.eot_token {
             let tokens_t = Tensor::new(tokens.as_slice(), audio_features.device())?;
             let tokens_t = tokens_t.unsqueeze(0)?;
@@ -323,13 +333,17 @@ impl Model {
             let logits = if let Some(lts) = last_timestamp {
                 self.supress_tokens(&logits, &tokens, lts)?
             } else {
-                // If this is the firs token, force it to be a timestamp, in the range: [0..1]
+                // If this is the first token, force it to be a timestamp, in the range: [0..1]
                 logits.broadcast_add(&self.first_token_supress)?
             };
 
             let next_token = if t > 0f64 {
                 let prs = softmax(&(&logits / t)?, 0).unwrap();
                 let logits_v: Vec<f32> = prs.to_vec1()?;
+                if !logits_v.iter().any(|x| !x.is_nan()) {
+                    tokens.push(self.eot_token);
+                    break;
+                }
                 let distr = rand::distributions::WeightedIndex::new(&logits_v).unwrap();
                 distr.sample(&mut self.rng) as u32
             } else {
